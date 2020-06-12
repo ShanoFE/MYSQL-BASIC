6010a4fa82d4d9e34870a32f5c81d58ceac928ec
Meshano Fransman <shano.fransman@student.rocva.nl>
2020-06-12 14:26:04 +0200

    Taak 1 en 2 af

--- 2-Select/taak04/antwoorden.sql
+++ 2-Select/taak04/antwoorden.sql
@@ -1,19 +1,20 @@
 -- Opdracht 1 
 SELECT MAX(wage) FROM players
 -- Opdracht 2 
-
+SELECT AVG(wage) FROM players
 -- Opdracht 3
-
+SELECT SUM(wage) FROM players
 -- Opdracht 4
-
+SELECT * FROM players WHERE club="Manchester City" OR club="Manchester United"
 -- Opdracht 5
-
+SELECT AVG(wage) FROM players WHERE nationality="Netherlands"
 -- Opdracht 6 
-
+SELECT AVG(wage) FROM players WHERE age<"20"
 -- Opdracht 7 
-
+SELECT AVG(wage) FROM players WHERE age>"20"
 -- Opdracht 8
-
+SELECT SUM(value) FROM players WHERE club="Chelsea"
 -- Opdracht 9
-
+SELECT ROUND(age) FROM players
 -- Opdracht 10
+SELECT club, wage, ROUND(value) FROM players WHERE club="Liverpool"
